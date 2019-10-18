8598befcbe9d19931f3392092f868de9365a9e17
Bryan Lopez <bryanlopezrr@gmail.com>
2019-10-18 18:56:17 -0400

    blank application and set up

---
+++ flixproject/flixprojectTests/flixprojectTests.swift
@@ -0,0 +1,34 @@
+//
+//  flixprojectTests.swift
+//  flixprojectTests
+//
+//  Created by Bryan Lopez on 10/18/19.
+//  Copyright © 2019 Bryan Lopez CP. All rights reserved.
+//
+
+import XCTest
+@testable import flixproject
+
+class flixprojectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
